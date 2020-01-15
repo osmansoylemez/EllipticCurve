74be3e0d33315b86c0d2205c2cbd1915031f2e5f
Osman SÖYLEMEZ <osmansoylemez@gmail.com>
2020-01-16 00:29:36 +0300

    first commit

---
+++ EllipticCurve/EllipticCurve/ECWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  ECWrapper.swift
+//  EllipticCurve
+//
+//  Created by Osman SÖYLEMEZ on 15.01.2020.
+//  Copyright © 2020 Osman SÖYLEMEZ. All rights reserved.
+//
+
+import UIKit
+
+class ECWrapper: NSObject {
+
+}
